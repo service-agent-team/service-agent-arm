9cc14a7cc27f2b4ccb4e41492ddd1e7514a7f178
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2023-12-05 17:07:51 +0500

    updated

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,20 +1,24 @@
-import { history } from '@/libs';
-import { Router } from '@/router';
+import { Loading } from '@/components';
+import { history, useBootstrap } from '@/libs';
+import { Routes } from '@/router';
 import { store } from '@/store';
-import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 import { HistoryRouter } from './history-router';
 import { ThemeProvider } from './theme-provider';
 
 export const App = () => {
+  const { isAuth, isInitiated } = useBootstrap();
+
+  if (!isInitiated) {
+    return <Loading />;
+  }
   return (
-    <Provider store={store}>
-      <ThemeProvider>
-        <HistoryRouter history={history}>
-          <Router />
-          <Toaster />
-        </HistoryRouter>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider>
+      <HistoryRouter history={history}>
+        <Provider store={store}>
+          <Routes isAuth={isAuth} />
+        </Provider>
+      </HistoryRouter>
+    </ThemeProvider>
   );
 };
