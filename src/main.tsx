167c73ef0f33ff9441a1d7f0965667a3fefee2ed
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2023-12-11 19:31:15 +0500

    update/app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
+import { App } from '@/app';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { App } from '@/app';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
-
-console.log('ok');
 
 root.render(
   <Provider store={store}>
