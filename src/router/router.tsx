f91054470c9efcade72f28531d70d33428de71ba
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2023-12-13 16:01:17 +0500

    last commit

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,82 +1,82 @@
 import { Loading } from '@/components';
 import { ROUTES } from '@/constants';
-import { GlobalLayout, PublicLayout } from '@/layouts';
+import { GlobalLayout, PrivateLayout, PublicLayout } from '@/layouts';
 import { Suspense } from 'react';
 import { Navigate, useRoutes } from 'react-router-dom';
 import { Home, Permissions, Projects, SignIn, UserPermissions, UserRoles, Users } from './loadable';
 
 export const Routes = ({ isAuth }: { isAuth: boolean }) =>
   useRoutes([
-    // {
-    //   element: <PrivateLayout isAuth={isAuth} />,
-    //   path: ROUTES.home,
-    //   children: [
     {
+      element: <PrivateLayout isAuth={isAuth} />,
       path: ROUTES.home,
-      element: <GlobalLayout isAuth={isAuth} />,
       children: [
-        { path: '*', element: <Navigate to={ROUTES.main} /> },
-        { index: true, element: <Navigate to={ROUTES.main} replace /> },
         {
-          path: ROUTES.main,
-          element: (
-            <Suspense fallback={<Loading />}>
-              <Home />
-            </Suspense>
-          ),
-        },
-        {
-          path: ROUTES.users,
-          element: (
-            <Suspense fallback={<Loading />}>
-              <Users />
-            </Suspense>
-          ),
-        },
-        {
-          path: ROUTES.permissions,
-          element: (
-            <Suspense fallback={<Loading />}>
-              <Permissions />
-            </Suspense>
-          ),
-        },
-        {
-          path: ROUTES.roles,
-          element: (
-            <Suspense fallback={<Loading />}>
-              <Permissions />
-            </Suspense>
-          ),
-        },
-        {
-          path: ROUTES.userRoles,
-          element: (
-            <Suspense fallback={<Loading />}>
-              <UserRoles />
-            </Suspense>
-          ),
-        },
-        {
-          path: ROUTES.userPermission,
-          element: (
-            <Suspense fallback={<Loading />}>
-              <UserPermissions />
-            </Suspense>
-          ),
-        },
-        {
-          path: ROUTES.projects,
-          element: (
-            <Suspense fallback={<Loading />}>
-              <Projects />
-            </Suspense>
-          ),
+          path: ROUTES.home,
+          element: <GlobalLayout isAuth={isAuth} />,
+          children: [
+            { path: '*', element: <Navigate to={ROUTES.main} /> },
+            { index: true, element: <Navigate to={ROUTES.main} replace /> },
+            {
+              path: ROUTES.main,
+              element: (
+                <Suspense fallback={<Loading />}>
+                  <Home />
+                </Suspense>
+              ),
+            },
+            {
+              path: ROUTES.users,
+              element: (
+                <Suspense fallback={<Loading />}>
+                  <Users />
+                </Suspense>
+              ),
+            },
+            {
+              path: ROUTES.permissions,
+              element: (
+                <Suspense fallback={<Loading />}>
+                  <Permissions />
+                </Suspense>
+              ),
+            },
+            {
+              path: ROUTES.roles,
+              element: (
+                <Suspense fallback={<Loading />}>
+                  <Permissions />
+                </Suspense>
+              ),
+            },
+            {
+              path: ROUTES.userRoles,
+              element: (
+                <Suspense fallback={<Loading />}>
+                  <UserRoles />
+                </Suspense>
+              ),
+            },
+            {
+              path: ROUTES.userPermission,
+              element: (
+                <Suspense fallback={<Loading />}>
+                  <UserPermissions />
+                </Suspense>
+              ),
+            },
+            {
+              path: ROUTES.projects,
+              element: (
+                <Suspense fallback={<Loading />}>
+                  <Projects />
+                </Suspense>
+              ),
+            },
+          ],
         },
       ],
     },
-    //   ],
-    // },
     {
       element: <PublicLayout isAuth={isAuth} />,
       children: [
