b8a9397d9dc761945dc1d704e51aa6de08b4d5aa
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2023-12-15 16:40:31 +0500

    code is updated

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@
 export { PrimaryBtn } from './PrimaryBtn';
 export { Icon } from './common/icon';
 export { Table } from './common/table';
-export { ContentCenter } from './content';
+export { ContentCenter } from './content/index';
 export { Loading } from './loading';
 export { PageTitle } from './page-title';
 // All exports
