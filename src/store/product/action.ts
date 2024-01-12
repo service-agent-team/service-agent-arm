0e71e12c564447ecf72bae011807cd5a5ab10050
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2024-01-12 23:07:38 +0500

    update

--- src/store/product/action.ts
+++ src/store/product/action.ts
@@ -4,16 +4,18 @@ import { ProductService } from '@/services';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { IProductPayload, IProductResponse } from './types';
 
-export const getByCategort = createAsyncThunk<IProductResponse,IProductPayload>('get/by/category', async ({categoryId,page,size callback }, thunkApi) => {
-  try {
-    const response = await ProductService.getByCategory(categoryId,page,size);
-
-    if (response.data.status == 200) {
-      callback();
+export const getByCategort = createAsyncThunk<IProductResponse, IProductPayload>(
+  'get/by/category',
+  async ({ categoryId, page, size, callback }, thunkApi) => {
+    try {
+      const response = await ProductService.getByCategory(categoryId, page, size);
+      if (response.data.status == 200) {
+        callback();
+      }
+      return response.data;
+    } catch (error) {
+      addNotification(error);
+      return thunkApi.rejectWithValue({ error: errorCatch(error) });
     }
-    return response.data;
-  } catch (error) {
-    addNotification(error);
-    return thunkApi.rejectWithValue({ error: errorCatch(error) });
-  }
-},);
+  },
+);
