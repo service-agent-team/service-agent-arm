edfdd705cc6523db83d2bafae1f7b0cf7c92b932
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2024-03-27 14:54:05 +0500

    service-agent home page chartjs

--- src/pages/service-agent/home/index.tsx
+++ src/pages/service-agent/home/index.tsx
@@ -1,5 +1,7 @@
-import { useActions } from '@/common/hooks';
+import { useActions, useTypedSelector } from '@/common/hooks';
+import * as S from './styled';
 import {
+  ArcElement,
   BarElement,
   CategoryScale,
   Chart as ChartJS,
@@ -9,9 +11,9 @@ import {
   Tooltip,
 } from 'chart.js';
 import { useEffect } from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Bar, Doughnut } from 'react-chartjs-2';
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement);
 export const options = {
   responsive: true,
   plugins: {
@@ -24,38 +26,78 @@ export const options = {
     },
   },
 };
-
-const labels = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Agent',
-      data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 10],
-      backgroundColor: '#5800FF',
+export const optionsSimCard = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
     },
-  ],
+    title: {
+      display: true,
+      text: 'Sim cards',
+    },
+  },
 };
 
 export const Home = () => {
   const { getAgentOrdersStatis } = useActions();
+  const { statistic } = useTypedSelector((state) => state.agentStatistic);
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const { active, all, esim, noresident, phisic, process, resident } = statistic || {};
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Agent',
+        data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 10],
+        backgroundColor: '#5800FF',
+      },
+    ],
+  };
+
+  const dataSimCards = {
+    labels: ['All', 'Active', 'E-sim', 'No resident', 'Physic', 'Process', 'Resident'],
+    datasets: [
+      {
+        label: 'Sim cards',
+        data: [all, active, esim, noresident, phisic, process, resident],
+        backgroundColor: [
+          '#ff6384',
+          '#36a2eb',
+          '#ffcd56',
+          '#ee0206',
+          '#22ff00',
+          '#676767',
+          '#a67400',
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
   useEffect(() => {
     getAgentOrdersStatis({ start: '12.01.2024', end: '12.02.2024' });
   }, [getAgentOrdersStatis]);
 
-  return <Bar options={options} data={data} />;
+  return (
+    <S.Block>
+      <Bar options={options} data={data} />
+      <Doughnut options={optionsSimCard} data={dataSimCards} />
+    </S.Block>
+  );
 };
