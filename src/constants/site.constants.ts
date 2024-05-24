8334b8b3636caa0c242a81921c122f49ea49a2f9
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2024-05-24 22:43:45 +0500

    fix: lets-trip tour locations

--- src/constants/site.constants.ts
+++ src/constants/site.constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://api.coreteam.uz'
-export const FILE_URL = 'https://files.coreteam.uz/api/v1/media/open'
+export const BASE_URL = 'https://api.coreteam.uz';
+export const FILE_URL = 'https://files.coreteam.uz/api/v1/media/open';
