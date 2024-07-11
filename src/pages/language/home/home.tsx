962f3ae41dabd783eb2f51e3a0efa077962cb163
Jasurbek002 <jasurbekturgunov227@gmail.com>
2024-07-11 11:30:48 +0500

    fix: language page

--- src/pages/language/home/home.tsx
+++ src/pages/language/home/home.tsx
@@ -2,9 +2,10 @@ export const LanguageHome = () => {
   return (
     <iframe
       src="https://language.coreteam.uz"
-      width="100wh"
+      width="100%"
       height="100vh"
       title="Let's Trip i18 language project"
+      style={{ border: 'none', height: '100vh' }}
     />
   );
 };
